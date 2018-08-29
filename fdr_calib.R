515ebbe1ef1cd961d53e0e9a352b502eae696623
Murilodsv <murilodsv@usp.br>
2018-08-29 16:09:49 -0300

    desc + format

--- fdr_calib.R
+++ fdr_calib.R
@@ -1,12 +1,38 @@
 #--- Sugarcane Water Use Data
 
+# Key equations:
+# sf  = a * swc ^ b + c
+# swc = 10 ^ (log10((sf-c)/a)/b)
+
+# Key variables:
+# rswc_sf  - fraction of level for swc in pmp and sat in respect to Scaled Frequency (SF) 
+# (i. e ): If all rswc_sf = 1, the calibration assumes that the minimum SF ~ Wilting Point (15000 Kpa) and the maximum SF ~ Saturation Point (10 Kpa)
+# ret_curv - retention curves data determined on the experimental area
+
+# History of events:
+# - Folegatti's Diviner 2000 was used on ratoon 1 and 2 seasons.
+# - A new Diviner 2000 acquired by GEPEMA was used for season 3
+# - Unfortunately, the Folegatti's Diviner broke down and no comparable measurements could be done
+# - Therefore, this code was developed to calibrate separately the Folegattis and GEPEMA Diviners 2000
+# - Calibration is assuming that 
+# - 1) Soil Water Content (SWC) can be computed from the Scaled Frenquencies (SF) based on the manufacturer equation (swc = 10 ^ (log10((sf-c)/a)/b))
+# - 2) The parameters abc can be calibrated to fit SF to retention curves measurements [c(min(SF),max(SF)) ~ c(min(SWC),max(SWC))]
+
+
+#--- IMPORTANT NOTES:
+#---   - SWC for Folgatti diviner equipment were already calibrated for depths 10-80cm. Here we calibrate the depth > 80cm
+#---   - SWC for GEPEMA diviner equipment were re-calibrated for all depths 10-150cm
+
+#-------------------------------------------------------------------------------
 
 wd = "d:/Murilo/exp_db/sugarcane/areao/fdr"
 plot_swc = F
+plot_adc = F
 export   = F
 opt_gep  = T
-opt_fol  = F
+opt_fol  = T
 opt_ly   = T
+data_suf = "v6"
 
 setwd(wd)
 
@@ -45,14 +71,14 @@ if(plot_swc){
           ylim= c(max(0,min(fdr_data[,feature])),max(fdr_data[,feature])),
           ylab= f_name,
           xlab= "Depth (cm)")
-
+  
   #--- Plot FDR data of GEPEMA Sensor
   boxplot(fdr_gep[,feature]~fdr_gep$depth,
           col = "lightgreen",
           ylim= c(max(0,min(fdr_data[,feature])),max(fdr_data[,feature])),
           ylab= f_name,
           xlab= "Depth (cm)")
-
+  
   fdr_fol_bp = bp_df(fdr_fol,T, y_lim = c(max(0,min(fdr_data[,feature])),max(fdr_data[,feature])), feat = feature,c("Depth (cm)",f_name))
   fdr_gep_bp = bp_df(fdr_gep,T, y_lim = c(max(0,min(fdr_data[,feature])),max(fdr_data[,feature])), feat = feature,c("Depth (cm)",f_name))
   
@@ -64,20 +90,20 @@ if(plot_swc){
 #--- retention curve indexing with FDR depth
 rc_depth = data.frame(depth_fdr   = seq(10,150,by = 10),
                       depth= c(    5, #10
-                                  15, #20
-                                  30, #30
-                                  30, #40
-                                  60, #50
-                                  60, #60
-                                  60, #70
-                                  100,#80
-                                  100,#90
-                                  100,#100
-                                  100,#110
-                                  100,#120
-                                  100,#130
-                                  100,#140
-                                  100))#150
+                                   15, #20
+                                   30, #30
+                                   30, #40
+                                   60, #50
+                                   60, #60
+                                   60, #70
+                                   100,#80
+                                   100,#90
+                                   100,#100
+                                   100,#110
+                                   100,#120
+                                   100,#130
+                                   100,#140
+                                   100))#150
 
 #--- indexer
 rc_idx = merge(ret_curv,rc_depth,by=c("depth"))
@@ -105,11 +131,11 @@ if(opt_gep){
       
       tb_dat = fdr_opt[fdr_opt$tube == tb,]
       
-      message(paste("Calibrating tube",tb))
-      
       #--- Calibrate for each depth SF
       l_dp = unique(tb_dat$depth)
       for(dp in l_dp){
+        
+        message(paste("Calibrating tube",tb,"at",dp,"cm depth (GEPEMA Diviner 2000)"))
         
         dp_dat = tb_dat[tb_dat$depth == dp,]
         
@@ -220,95 +246,143 @@ if(opt_gep){
     fdr_gep$C_calib = c
     fdr_gep$calib_meas = fdr_opt$calib_meas_new
   }
-
+  
 }
 
 #--------------------------
 
 if(opt_fol){
+  
   fdr_opt = fdr_fol
-  fdr_opt$calib_meas_new = fdr_opt$orig_meas
   
-  #--- Assuming that the frequency on top layers are equivalent to other layers:
-  #--- Assuming that the pmp ocurred at least once at 10 cm depth
-  min_sf = aggregate(sf ~ tube,fdr_opt[fdr_opt$depth==10,],min)
-  pmp_sf = quantile(min_sf$sf,0.25)[1]
+  #--------------------------------------------------
+  #--- calibrate only for bottom layer (dp > 80) ----
+  #--------------------------------------------------
+  #--- the layers 10-80 were already calibrated!!!
   
-  #--- Assuming that the saturation point is the maximun scaled frenquency measured on all series
-  max_sf = aggregate(sf ~ tube, fdr_opt,max)
-  sat_sf = quantile(max_sf$sf,0.75)[1]
+  fdr_opt = fdr_opt[fdr_opt$depth > 80,]
   
-  #--- Original parameters
-  a = unique(fdr_opt$A)
-  b = unique(fdr_opt$B)
-  c = unique(fdr_opt$C)
   
-  #--- add paramters to a single vector
-  p = c(a,b,c)
+  rswc_sf = data.frame(dp =          c( 10, 20, 30, 40, 50, 60, 70, 80, 90,100,110,120,130,140,150),
+                       rswc_sf_min = c(1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0),
+                       rswc_sf_max = c(1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0))
   
-  #--- opmtimze paramters
-  par_abc = optim(p,opt_fdr)
-  par_abc$par
-  
-  #--- Set of optmized parameters
-  a = par_abc$par[1]
-  b = par_abc$par[2]
-  c = par_abc$par[3]
-  
-  #--- Compute calibrated swc
-  fdr_opt$calib_meas_new = 10 ^ (log10((fdr_opt$sf-c)/a)/b)
-  
-  #--- write optimized parameters to original db
-  fdr_fol$A_calib = a
-  fdr_fol$B_calib = b
-  fdr_fol$C_calib = c
-  fdr_fol$calib_meas = fdr_opt$calib_meas_new
+  if(opt_ly){
+    
+    #--- Calibrate for each tube SF
+    l_tb = unique(fdr_opt$tube)
+    for(tb in l_tb){
+      
+      tb_dat = fdr_opt[fdr_opt$tube == tb,]
+      
+      #--- Calibrate for each depth SF
+      l_dp = unique(tb_dat$depth)
+      for(dp in l_dp){
+        
+        message(paste("Calibrating tube",tb,"at",dp,"cm depth (Folegatti Diviner 2000)"))
+        
+        dp_dat = tb_dat[tb_dat$depth == dp,]
+        
+        #--- Min and Max SF
+        min_sf = min(tb_dat$sf)
+        max_sf = max(tb_dat$sf)
+        
+        #--- Min and Max SWC from Retention Curves data
+        swc_min = rc_idx[rc_idx$depth_fdr == dp & rc_idx$h == 15000,"swc"] * rswc_sf$rswc_sf_min[rswc_sf$dp == dp] * 100
+        swc_max = rc_idx[rc_idx$depth_fdr == dp & rc_idx$h ==    10,"swc"] * rswc_sf$rswc_sf_max[rswc_sf$dp == dp] * 100
+        
+        a = unique(dp_dat$A)
+        b = unique(dp_dat$B)
+        c = unique(dp_dat$C)
+        
+        p = c(a,b,c)
+        
+        #--- opmtimze paramters to fit c(min_sf,max_sf) ~ c(swc_min,swc_max)
+        par_abc = optim(p,opt_fdr)
+        par_abc$par
+        
+        #--- update to optmized parameters
+        dp_dat$A_calib = par_abc$par[1]
+        dp_dat$B_calib = par_abc$par[2]
+        dp_dat$C_calib = par_abc$par[3]
+        
+        #--- join results by depth
+        if(dp == l_dp[1]){
+          dp_dat_opt = dp_dat
+        }else{
+          dp_dat_opt = rbind(dp_dat_opt,dp_dat)
+        }
+      }
+      
+      #--- join results by tube
+      if(tb == l_tb[1]){
+        fdr_opt_ly = dp_dat_opt
+      }else{
+        fdr_opt_ly = rbind(fdr_opt_ly,dp_dat_opt)
+      }
+    }
+    
+    #--- compute calibrated swc
+    a = fdr_opt_ly$A_calib
+    b = fdr_opt_ly$B_calib
+    c = fdr_opt_ly$C_calib
+    sf= fdr_opt_ly$sf
+    
+    fdr_opt_ly$calib_meas = 10 ^ (log10((sf-c)/a)/b)
+    
+    #--- update calibrated swc for dp>80cm
+    fdr_fol = rbind(fdr_fol[fdr_fol$depth <= 80,],fdr_opt_ly)
+  }else{
+    
+    
+    #--- Assuming that the frequency on top layers are equivalent to other layers:
+    #--- Assuming that the pmp ocurred at least once at 10 cm depth
+    min_sf = aggregate(sf ~ tube,fdr_opt[fdr_opt$depth==10,],min)
+    pmp_sf = quantile(min_sf$sf,0.25)[1]
+    
+    #--- Assuming that the saturation point is the maximun scaled frenquency measured on all series
+    max_sf = aggregate(sf ~ tube, fdr_opt,max)
+    sat_sf = quantile(max_sf$sf,0.75)[1]
+    
+    #--- Original parameters
+    a = unique(fdr_opt$A)
+    b = unique(fdr_opt$B)
+    c = unique(fdr_opt$C)
+    
+    #--- add paramters to a single vector
+    p = c(a,b,c)
+    
+    #--- opmtimze paramters
+    par_abc = optim(p,opt_fdr)
+    par_abc$par
+    
+    #--- Set of optmized parameters
+    a = par_abc$par[1]
+    b = par_abc$par[2]
+    c = par_abc$par[3]
+    
+    #--- Compute calibrated swc
+    fdr_opt$calib_meas_new = 10 ^ (log10((fdr_opt$sf-c)/a)/b)
+    
+    #--- write optimized parameters to original db
+    fdr_fol$A_calib = a
+    fdr_fol$B_calib = b
+    fdr_fol$C_calib = c
+    fdr_fol$calib_meas = fdr_opt$calib_meas_new
+  }
 }
 
 #--------------------------
 
-
-
+#--- update both folegatti and gepemas equipment caliration data
 fdr_data_calib = rbind(fdr_fol,fdr_gep)
 
-
-
-
-
-# #--- Compute FDR data for Folegatti equipment for bottom soil (depth >80 cm)
-# #--- Assume same a,b c parameters as used for 80 cm
-# 
-# l_tb = unique(fdr_fol$tube)
-# for(tb in l_tb){
-#   
-#   #--- 80cm parameters
-#   a   = unique(fdr_fol$A_calib[fdr_fol$tube==tb & fdr_fol$depth==80])
-#   b   = unique(fdr_fol$B_calib[fdr_fol$tube==tb & fdr_fol$depth==80])
-#   c   = unique(fdr_fol$C_calib[fdr_fol$tube==tb & fdr_fol$depth==80])
-#   sf  = fdr_fol$sf[fdr_fol$tube==tb & fdr_fol$depth > 80] 
-#   
-#   fdr_fol$calib_meas[fdr_fol$tube==tb & fdr_fol$depth > 80] = 10 ^ (log10((sf-c)/a)/b)
-#   
-#   #--- assign abc parameters to fol data
-#   fdr_fol$A_calib[fdr_fol$tube==tb & fdr_fol$depth > 80] = a
-#   fdr_fol$B_calib[fdr_fol$tube==tb & fdr_fol$depth > 80] = b
-#   fdr_fol$C_calib[fdr_fol$tube==tb & fdr_fol$depth > 80] = c
-#   
-# }
-
-
-
+#--- plot data
 l_dp = c(10,30,60,90,120,150)
 colors = data.frame(depth = l_dp,
                     colors= topo.colors(n = length(l_dp), alpha = 1))
 
-colnames(fdr_data_calib)
 fdr_data_calib = fdr_data_calib[fdr_data_calib$calib_meas>0,]
-
-max(fdr_data_calib$calib_meas)
-
-boxplot(fdr_data_calib$calib_meas)
-boxplot(fdr_data_calib$sf)
 
 for(dp in l_dp){
   ad = T
@@ -319,117 +393,100 @@ for(dp in l_dp){
 legend("topright",inset = 0.03,legend = l_dp,lt = rep(1,length(colors$colors)),col = as.character(colors$colors))
 
 #--- write optmized values
-write.csv(fdr_data_calib[,c("date","responsable","season","equip_owner","treat","tube","position","depth","orig_meas","A_calib","B_calib","C_calib","calib_meas")],file = "fdr_data_calib_v3.csv", row.names = F)
-colnames(fdr_data_calib)
+filename = paste0("fdr_data_calib_",data_suf,".csv")
+write.csv(fdr_data_calib,file = filename, row.names = F)
 
 
+message(paste("Calibration saved on file",filename))
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-feature = "calib_meas"
-
-for(tb in unique(fdr_opt$tube)){
-  png(paste0("FDR_calib_tube_",tb,".png"),units="in",width=12,height=12,pointsize=15,res=300)
-  colors = data.frame(depth = unique(fdr_opt$depth[fdr_opt$tube==tb]),
-                      colors= heat.colors(n = length(unique(fdr_opt$depth[fdr_opt$tube == tb])), alpha = 1))
+if(plot_adc){
   
-  plot(fdr_opt$calib_meas_new[fdr_opt$tube==tb & fdr_opt$depth==10]~fdr_opt$date[fdr_opt$tube==tb & fdr_opt$depth==10],
-       type = "b",
-       ylim = c(0.15,0.50),
-       ylab = "SWC (cm3 cm-3)",
-       xlab = "Date")
+  # WARNING THIS FOLLOWING CODE WAS NOT TOTALLY TESTED (DEBUGGIN PURPOSE)
+  # KEEP plot_adc = F
   
-  for(d in unique(fdr_opt$depth[fdr_opt$tube==tb])){
+  
+  
+  feature = "calib_meas"
+  
+  for(tb in unique(fdr_opt$tube)){
+    png(paste0("FDR_calib_tube_",tb,".png"),units="in",width=12,height=12,pointsize=15,res=300)
+    colors = data.frame(depth = unique(fdr_opt$depth[fdr_opt$tube==tb]),
+                        colors= heat.colors(n = length(unique(fdr_opt$depth[fdr_opt$tube == tb])), alpha = 1))
     
-    lines(fdr_opt$calib_meas_new[fdr_opt$tube==tb & fdr_opt$depth==d]~fdr_opt$date[fdr_opt$tube==tb & fdr_opt$depth==d],
-          type = "b",
-          ylim = c(0.15,0.50),
-          col = as.character(colors$colors[colors$depth==d]))
+    plot(fdr_opt$calib_meas_new[fdr_opt$tube==tb & fdr_opt$depth==10]~fdr_opt$date[fdr_opt$tube==tb & fdr_opt$depth==10],
+         type = "b",
+         ylim = c(0.15,0.50),
+         ylab = "SWC (cm3 cm-3)",
+         xlab = "Date")
     
+    for(d in unique(fdr_opt$depth[fdr_opt$tube==tb])){
+      
+      lines(fdr_opt$calib_meas_new[fdr_opt$tube==tb & fdr_opt$depth==d]~fdr_opt$date[fdr_opt$tube==tb & fdr_opt$depth==d],
+            type = "b",
+            ylim = c(0.15,0.50),
+            col = as.character(colors$colors[colors$depth==d]))
+      
+    }
+    
+    legend("bottomright",legend = unique(fdr_opt$depth[fdr_opt$tube==tb]),col = as.character(colors$colors), lt = rep(1,length(colors$colors)))
+    dev.off()
   }
   
-  legend("bottomright",legend = unique(fdr_opt$depth[fdr_opt$tube==tb]),col = as.character(colors$colors), lt = rep(1,length(colors$colors)))
-  dev.off()
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-tb1 = 69
-tb2 = 68
-d = 50
-
-t1_t2 = merge(fdr_opt[fdr_opt$tube==tb1 & fdr_opt$depth==d,c("date","calib_meas_new")],
-      fdr_opt[fdr_opt$tube==tb2 & fdr_opt$depth==d,c("date","calib_meas_new")],
-      by = "date")
-
-colnames(t1_t2) = c("date","tb1","tb2")
-
-mperf(sim = t1_t2[,2], obs = t1_t2[,3],vnam = "swc")
-
-for(sd in l_sd){
-  for(tb in l_tb){
-    if(length(fdr_opt[fdr_opt$tube==tb & fdr_opt$depth==sd,"calib_meas_new"]) > 0){
-      
-      min_swc = min(fdr_opt[fdr_opt$tube==tb & fdr_opt$depth==sd,"calib_meas_new"])
-      max_swc = max(fdr_opt[fdr_opt$tube==tb & fdr_opt$depth==sd,"calib_meas_new"])
-    
+  
+  
+  tb1 = 69
+  tb2 = 68
+  d = 50
+  
+  t1_t2 = merge(fdr_opt[fdr_opt$tube==tb1 & fdr_opt$depth==d,c("date","calib_meas_new")],
+                fdr_opt[fdr_opt$tube==tb2 & fdr_opt$depth==d,c("date","calib_meas_new")],
+                by = "date")
+  
+  colnames(t1_t2) = c("date","tb1","tb2")
+  
+  mperf(sim = t1_t2[,2], obs = t1_t2[,3],vnam = "swc")
+  
+  for(sd in l_sd){
+    for(tb in l_tb){
+      if(length(fdr_opt[fdr_opt$tube==tb & fdr_opt$depth==sd,"calib_meas_new"]) > 0){
+        
+        min_swc = min(fdr_opt[fdr_opt$tube==tb & fdr_opt$depth==sd,"calib_meas_new"])
+        max_swc = max(fdr_opt[fdr_opt$tube==tb & fdr_opt$depth==sd,"calib_meas_new"])
+        
+      }
     }
   }
-}
-
-
-
-
-
-
-write.csv(fdr_fol_bp, file = "fdr_fol_bp.csv", row.names = F)
-write.csv(fdr_gep_bp, file = "fdr_gep_bp.csv", row.names = F)
-
-fdr_sd_ag    = aggregate(freq ~ date+depth, data = fdr_data, sd)
-fdr_mean_ag  = aggregate(freq ~ date+depth, data = fdr_data, mean)
-
-dp = 10
-
-fdr_mean  = fdr_mean_ag[fdr_mean_ag$depth==dp,]
-fdr_sd    = fdr_sd_ag[fdr_sd_ag$depth==dp,]
-
-ybot = fdr_mean[,3]-fdr_sd[,3]
-yupp = fdr_mean[,3]+fdr_sd[,3]
-
-plot(fdr_mean[,3]~fdr_mean[,1],
-     ylim = c(min(ybot),max(yupp)))
-     
-arrows(fdr_mean[,1],
-       ybot,
-       fdr_mean[,1],
-       yupp,
-       length=0.02, angle=90, code=3)
-
-
+  
+  
+  
+  
+  
+  
+  write.csv(fdr_fol_bp, file = "fdr_fol_bp.csv", row.names = F)
+  write.csv(fdr_gep_bp, file = "fdr_gep_bp.csv", row.names = F)
+  
+  fdr_sd_ag    = aggregate(freq ~ date+depth, data = fdr_data, sd)
+  fdr_mean_ag  = aggregate(freq ~ date+depth, data = fdr_data, mean)
+  
+  dp = 10
+  
+  fdr_mean  = fdr_mean_ag[fdr_mean_ag$depth==dp,]
+  fdr_sd    = fdr_sd_ag[fdr_sd_ag$depth==dp,]
+  
+  ybot = fdr_mean[,3]-fdr_sd[,3]
+  yupp = fdr_mean[,3]+fdr_sd[,3]
+  
+  plot(fdr_mean[,3]~fdr_mean[,1],
+       ylim = c(min(ybot),max(yupp)))
+  
+  arrows(fdr_mean[,1],
+         ybot,
+         fdr_mean[,1],
+         yupp,
+         length=0.02, angle=90, code=3)
+  
+  
   avg = rnorm(10,5,1)
   sdev = rnorm(10,0.1,0.01)
   plot(avg)
@@ -437,38 +494,52 @@ arrows(fdr_mean[,1],
   arrows(seq(1,10), avg-sdev, seq(1,10), avg+sdev, length=0.05, angle=90, code=3)
   
   plot(fdr_data$calib_meas~fdr_data$date)
-
-mperf(fdr_fol_bp$b1,fdr_gep_bp$b1,vnam = "swc",dchart = T, outidx = "rmse")
-
-hist(fdr_fol$calib_meas[fdr_fol$depth==50])
-hist(fdr_gep$calib_meas[fdr_gep$depth==50])
-
-
-
-
-
-
-
-
-bp = boxplot(fdr_dt$calib_meas~fdr_dt$tube, plot = F)
-bp_df$names
-
-for(t in tube_l){
   
-
-  bp_df = data.frame(tube = t,
-                     b1   = bp$stats[1,])
+  mperf(fdr_fol_bp$b1,fdr_gep_bp$b1,vnam = "swc",dchart = T, outidx = "rmse")
+  
+  hist(fdr_fol$calib_meas[fdr_fol$depth==50])
+  hist(fdr_gep$calib_meas[fdr_gep$depth==50])
+  
+  
+  bp = boxplot(fdr_dt$calib_meas~fdr_dt$tube, plot = F)
+  bp_df$names
+  
+  for(t in tube_l){
+    
+    
+    bp_df = data.frame(tube = t,
+                       b1   = bp$stats[1,])
+    
+  }
+  
+  a   = fdr_data$A
+  b   = fdr_data$B
+  c   = fdr_data$C
+  swc = fdr_data$Leitura.Calibrada_Tese_Murilo
+  
+  sf  = a * swc ^ b + c
+  swc = 10 ^ (log10((sf-c)/a)/b)
+  
+  plot(sf~fdr_data$date, type = "l")
+  
+  unique(fdr_data$A)
+  
+  
   
 }
 
-a   = fdr_data$A
-b   = fdr_data$B
-c   = fdr_data$C
-swc = fdr_data$Leitura.Calibrada_Tese_Murilo
 
-sf  = a * swc ^ b + c
-swc = 10 ^ (log10((sf-c)/a)/b)
 
-plot(sf~fdr_data$date, type = "l")
 
-unique(fdr_data$A)
+
+
+
+
+
+
+
+
+
+
+
+
